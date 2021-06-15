c9b2a7244cee00644b5d63e3aa06282234675787
gustavklopp <gustavklopp@protonmail.com>
2021-06-15 14:49:55 +0200

    help for MacOS

--- lwt/views/helppage.py
+++ lwt/views/helppage.py
@@ -2,6 +2,8 @@
 # django:
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+# third party
+import platform
 # helper functions:
 from lwt.views._nolang_redirect_decorator import *
 from lwt.views._utilities_views import get_word_database_size
@@ -14,5 +16,12 @@ from lwt.views._utilities_views import get_word_database_size
 def helppage(request):
     # get the current database size:
     database_size = get_word_database_size(request)
- 
-    return render(request, 'lwt/helppage.html',{'database_size':database_size})
+
+    # detect if mac or else
+    system = platform.system().lower()
+    if system == 'windows' or system == 'linux': 
+        is_Mac = False
+    else:
+        is_Mac = True
+
+    return render(request, 'lwt/helppage.html',{'is_Mac':is_Mac, 'database_size':database_size})
